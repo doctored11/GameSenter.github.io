b7a7504919d73491dd79cba76f0367142fb6ab83
drEd11 <93774837+doctored11@users.noreply.github.com>
2022-05-24 21:37:14 +0300

    update

---
+++ js/script.js
@@ -0,0 +1,10 @@
+let card = document.querySelectorAll(".header__nav-item");
+
+let logo = document.querySelector(".card__logo");
+// card.forEach(card => card.addEventListener('mousemove', animUp()));
+// function animUp(){
+//   card.classList.add("up-img");
+// }
+card.forEach(function(){
+  logo.classList.add('а');
+}) ;
